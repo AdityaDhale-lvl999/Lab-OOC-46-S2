7d86aef7e1e9145f0a0718e14577bd65e2194df8
Aditya Dhale <97932776+AdityaDhale-lvl999@users.noreply.github.com>
2026-08-24 05:11:17 +0000

    epx 5

---
+++ Experiment-5/rect_c.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Rectangle {
+    public :
+     float area;
+     Rectangle () {
+        area = 0;
+
+     }
+     Rectangle (int a , int b){
+        area = a * b;
+
+     }
+     void disp(){
+        cout << area << endl;
+     }
+};
+int main (){
+    Rectangle O;
+    Rectangle O2 (10 , 20);
+    O.disp();
+    O2.disp();
+    return 1;
+
+}
